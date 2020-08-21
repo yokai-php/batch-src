d164069d99efd8739dc2f8034acf45451da294d8
Yann Eugoné <yeugone@prestaconcept.net>
2020-08-21 19:50:15 +0200

    Fix monorepo

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -8,7 +8,7 @@ use Symplify\MonorepoBuilder\ValueObject\Option;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
-    $parameters->set(Option::PACKAGE_DIRECTORIES, 'src');
+    $parameters->set(Option::PACKAGE_DIRECTORIES, ['src']);
     $parameters->set(Option::DIRECTORIES_TO_REPOSITORIES, [
         'src/*' => 'git@github.com:yokai-php/*.git',
     ]);
