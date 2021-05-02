5e64a1f7e714900a0313a73ae22796288f94b6cb
Yann Eugoné <yeugone@prestaconcept.net>
2021-05-02 15:04:03 +0200

    Very simple release config

--- monorepo-builder.php
+++ monorepo-builder.php
@@ -3,6 +3,9 @@
 declare(strict_types=1);
 
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
 use Symplify\MonorepoBuilder\ValueObject\Option;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
@@ -20,4 +23,11 @@ return static function (ContainerConfigurator $containerConfigurator): void {
             ],
         ],
     ]);
+
+    $services = $containerConfigurator->services();
+
+    # release workers - in order to execute
+    $services->set(SetCurrentMutualDependenciesReleaseWorker::class);
+    $services->set(TagVersionReleaseWorker::class);
+    $services->set(PushTagReleaseWorker::class);
 };
