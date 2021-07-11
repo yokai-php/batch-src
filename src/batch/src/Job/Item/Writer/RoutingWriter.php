9474e886d15e3a918f194b9db78981c8cbd16f25
Yann Eugoné <yeugone@prestaconcept.net>
2021-07-11 19:30:10 +0200

    Create routing item writer

---
+++ src/batch/src/Job/Item/Writer/RoutingWriter.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yokai\Batch\Job\Item\Writer;
+
+use Yokai\Batch\Job\Item\ElementConfiguratorTrait;
+use Yokai\Batch\Job\Item\FlushableInterface;
+use Yokai\Batch\Job\Item\InitializableInterface;
+use Yokai\Batch\Job\Item\ItemWriterInterface;
+use Yokai\Batch\Job\JobExecutionAwareInterface;
+use Yokai\Batch\Job\JobExecutionAwareTrait;
+use Yokai\Batch\Routing\RoutingInterface;
+
+final class RoutingWriter implements
+    ItemWriterInterface,
+    InitializableInterface,
+    FlushableInterface,
+    JobExecutionAwareInterface
+{
+    use ElementConfiguratorTrait;
+    use JobExecutionAwareTrait;
+
+    private RoutingInterface $routing;
+
+    /**
+     * @var ItemWriterInterface[]
+     */
+    private array $writers = [];
+
+    public function __construct(RoutingInterface $routing)
+    {
+        $this->routing = $routing;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function write(iterable $items): void
+    {
+        $writerAndItems = [];
+        foreach ($items as $item) {
+            /** @var ItemWriterInterface $writer */
+            $writer = $this->routing->get($item);
+
+            $writerId = \spl_object_hash($writer);
+
+            if (!isset($this->writers[$writerId])) {
+                $this->writers[$writerId] = $writer;
+                $this->configureElementJobContext($writer, $this->jobExecution);
+                $this->initializeElement($writer);
+            }
+
+            $writerAndItems[$writerId] ??= [$writer, []];
+            $writerAndItems[$writerId][1][] = $item;
+        }
+
+        foreach ($writerAndItems as [$writer, $writerItems]) {
+            $writer->write($writerItems);
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function initialize(): void
+    {
+        $this->writers = [];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function flush(): void
+    {
+        $writers = $this->writers;
+        $this->writers = [];
+
+        foreach ($writers as $writer) {
+            $this->flushElement($writer);
+        }
+    }
+}
