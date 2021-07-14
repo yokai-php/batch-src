4db49599b2009882148c5e22d278f426de5bb107
Yann Eugoné <yeugone@prestaconcept.net>
2021-07-14 11:28:31 +0200

    Created DoctrineDBALInsertWriter, an item writer that writes array data to an SQL table

---
+++ src/batch-doctrine-dbal/tests/DoctrineDBALInsertWriterTest.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yokai\Batch\Tests\Bridge\Doctrine\DBAL;
+
+use Doctrine\DBAL\Types\Types;
+use Yokai\Batch\Bridge\Doctrine\DBAL\DoctrineDBALInsertWriter;
+
+class DoctrineDBALInsertWriterTest extends DoctrineDBALTestCase
+{
+    public function test(): void
+    {
+        $this->createTable('persons', [
+            'firstName' => Types::STRING,
+            'lastName' => Types::STRING,
+        ]);
+
+        $writer = new DoctrineDBALInsertWriter($this->doctrine, 'persons');
+
+        $writer->write([
+            ['firstName' => 'John', 'lastName' => 'Doe'],
+            ['firstName' => 'Jane', 'lastName' => 'Doe'],
+        ]);
+        self::assertSame([
+            ['firstName' => 'John', 'lastName' => 'Doe'],
+            ['firstName' => 'Jane', 'lastName' => 'Doe'],
+        ], $this->findAll('persons'));
+
+        $writer->write([
+            ['firstName' => 'Jack', 'lastName' => 'Doe'],
+        ]);
+        self::assertSame([
+            ['firstName' => 'John', 'lastName' => 'Doe'],
+            ['firstName' => 'Jane', 'lastName' => 'Doe'],
+            ['firstName' => 'Jack', 'lastName' => 'Doe'],
+        ], $this->findAll('persons'));
+    }
+}
