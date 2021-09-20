8937739b28aaa734ac88366caa4af38fcd350a45
Yann Eugoné <yeugone@prestaconcept.net>
2021-09-20 18:17:06 +0200

    Add documentation to static constructors

--- src/batch/src/Job/Item/Reader/IndexWithReader.php
+++ src/batch/src/Job/Item/Reader/IndexWithReader.php
@@ -37,16 +37,34 @@ final class IndexWithReader implements
         $this->extractItemIndex = $extractItemIndex;
     }
 
+    /**
+     * Uses item array value as the item index.
+     *
+     * Example, IndexWithReader::withArrayKey(..., 'name')
+     * will use 'name' array index of each read item as the item index.
+     */
     public static function withArrayKey(ItemReaderInterface $reader, string $key): self
     {
         return new self($reader, fn(array $item) => $item[$key]);
     }
 
+    /**
+     * Uses object property value as the item index.
+     *
+     * Example, IndexWithReader::withProperty(..., 'name')
+     * will use 'name' object property of each read item as the item index.
+     */
     public static function withProperty(ItemReaderInterface $reader, string $property): self
     {
         return new self($reader, fn(object $item) => $item->$property);
     }
 
+    /**
+     * Uses object method return value as the item index.
+     *
+     * Example, IndexWithReader::withProperty(..., 'getName')
+     * will call 'getName()' method of each read item and uses the result as the item index.
+     */
     public static function withGetter(ItemReaderInterface $reader, string $getter): self
     {
         return new self($reader, fn(object $item) => $item->$getter());
