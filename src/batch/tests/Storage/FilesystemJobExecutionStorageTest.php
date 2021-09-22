c34e25077c98425ecfc34f055a3141a5ac95f499
Yann Eugoné <yeugone@prestaconcept.net>
2021-09-22 19:15:25 +0200

    Add more tests to cover edge cases of filesystem job execution storage

--- src/batch/tests/Storage/FilesystemJobExecutionStorageTest.php
+++ src/batch/tests/Storage/FilesystemJobExecutionStorageTest.php
@@ -13,6 +13,7 @@ use Yokai\Batch\Exception\CannotStoreJobExecutionException;
 use Yokai\Batch\Exception\JobExecutionNotFoundException;
 use Yokai\Batch\JobExecution;
 use Yokai\Batch\Serializer\JobExecutionSerializerInterface;
+use Yokai\Batch\Serializer\JsonJobExecutionSerializer;
 use Yokai\Batch\Storage\FilesystemJobExecutionStorage;
 use Yokai\Batch\Storage\Query;
 use Yokai\Batch\Storage\QueryBuilder;
@@ -22,6 +23,7 @@ class FilesystemJobExecutionStorageTest extends TestCase
     use ProphecyTrait;
 
     private const STORAGE_DIR = ARTIFACT_DIR . '/filesystem-storage';
+    private const READONLY_STORAGE_DIR = __DIR__ . '/fixtures/filesystem-job-execution-readonly';
 
     /**
      * @var JobExecutionSerializerInterface|ObjectProphecy
@@ -40,10 +42,12 @@ class FilesystemJobExecutionStorageTest extends TestCase
         unset($this->serializer);
     }
 
-    private function createStorage(string $dir = self::STORAGE_DIR)
-    {
+    private function createStorage(
+        string $dir = self::STORAGE_DIR,
+        JobExecutionSerializerInterface $serializer = null
+    ): FilesystemJobExecutionStorage {
         return new FilesystemJobExecutionStorage(
-            $this->serializer->reveal(),
+            $serializer ?? $this->serializer->reveal(),
             $dir
         );
     }
@@ -62,6 +66,18 @@ class FilesystemJobExecutionStorageTest extends TestCase
         self::assertFileExists($file);
         self::assertIsReadable($file);
         self::assertEquals('serialized job execution', file_get_contents($file));
+    }
+
+    public function testStoreFileNotWritable(): void
+    {
+        $this->expectException(CannotStoreJobExecutionException::class);
+
+        $jobExecution = JobExecution::createRoot('123456789', 'export');
+        $this->serializer->serialize($jobExecution)
+            ->shouldBeCalledTimes(1)
+            ->willReturn('serialized job execution');
+
+        $this->createStorage(self::READONLY_STORAGE_DIR)->store($jobExecution);
     }
 
     public function testStoreFilePathNotFound(): void
@@ -85,102 +101,121 @@ class FilesystemJobExecutionStorageTest extends TestCase
         self::assertSame($jobExecution, $this->createStorage()->retrieve('export', '123456789'));
     }
 
-    public function testList(): void
+    /**
+     * @dataProvider list
+     */
+    public function testList(string $jobName, array $expectedCouples): void
     {
-        $dir = self::STORAGE_DIR . '/list';
+        $storage = $this->createStorage(
+            __DIR__ . '/fixtures/filesystem-job-execution',
+            new JsonJobExecutionSerializer()
+        );
 
-        $expected = [];
-        foreach (['import', 'export'] as $jobName) {
-            @mkdir($dir . "/$jobName", 0755, true);
-            foreach (['123', '456'] as $executionId) {
-                $execution = JobExecution::createRoot($executionId, $jobName);
-                if ($jobName === 'export') {
-                    $expected[] = $execution;
-                }
-                file_put_contents($dir . "/$jobName/$executionId.txt", "$jobName/$executionId");
-                $this->serializer->unserialize("$jobName/$executionId")
-                    ->shouldBeCalledTimes($jobName === 'export' ? 1 : 0)
-                    ->willReturn($execution);
-            }
-        }
-
-        /** @var \Iterator $exports */
-        $exports = $this->createStorage($dir)->list('export');
-        self::assertInstanceOf(\Iterator::class, $exports);
-        self::assertSame($expected, iterator_to_array($exports));
+        self::assertExecutions($expectedCouples, $storage->list($jobName));
     }
 
-    public function testQuery(): void
+    public function list(): \Generator
     {
-        $dir = self::STORAGE_DIR . '/query';
-        $storage = $this->createStorage($dir);
+        yield [
+            'export',
+            [
+                ['export', '20210920'],
+                ['export', '20210922'],
+            ],
+        ];
+        yield [
+            'list',
+            [
+                ['list', '20210910'],
+                ['list', '20210915'],
+                ['list', '20210920'],
+            ],
+        ];
+    }
 
-        /** @var JobExecution[] $executions */
-        $completedImport2017 = JobExecution::createRoot(
-            '123',
-            'query-import',
-            new BatchStatus(BatchStatus::COMPLETED)
+    /**
+     * @dataProvider query
+     */
+    public function testQueryWithProvider(QueryBuilder $query, array $expectedCouples): void
+    {
+        $storage = $this->createStorage(
+            __DIR__ . '/fixtures/filesystem-job-execution',
+            new JsonJobExecutionSerializer()
         );
-        $completedImport2017->setStartTime(new \DateTimeImmutable('2017-01-01 12:00:00'));
-        $completedImport2017->setEndTime(new \DateTimeImmutable('2017-01-01 13:00:00'));
 
-        $failedImport2018 = JobExecution::createRoot(
-            '456',
-            'query-import',
-            new BatchStatus(BatchStatus::FAILED)
-        );
-        $failedImport2018->setStartTime(new \DateTimeImmutable('2018-01-01 12:00:00'));
-        $failedImport2018->setEndTime(new \DateTimeImmutable('2018-01-01 13:00:00'));
+        self::assertExecutions($expectedCouples, $storage->query($query->getQuery()));
+    }
 
-        $pendingExport2019 = JobExecution::createRoot(
-            '123',
-            'query-export',
-            new BatchStatus(BatchStatus::PENDING)
-        );
-
-        $runningExport2020 = JobExecution::createRoot(
-            '456',
-            'query-export',
-            new BatchStatus(BatchStatus::RUNNING)
-        );
-        $runningExport2020->setStartTime(new \DateTimeImmutable('2020-01-01 12:00:00'));
-
-        $executions = [$completedImport2017, $failedImport2018, $pendingExport2019, $runningExport2020];
-        foreach ($executions as $execution) {
-            $jobName = $execution->getJobName();
-            $executionId = $execution->getId();
-
-            @mkdir($dir . "/$jobName", 0755, true);
-            file_put_contents($dir . "/$jobName/$executionId.txt", "$jobName/$executionId");
-            $this->serializer->unserialize("$jobName/$executionId")
-                ->willReturn($execution);
-        }
-
-        $runningOrPendingRecentlyStarted = $storage->query(
+    public function query(): \Generator
+    {
+        yield 'Filter ids' => [
             (new QueryBuilder())
-                ->statuses([BatchStatus::RUNNING, BatchStatus::PENDING])
-                ->sort(Query::SORT_BY_START_DESC)
-                ->getQuery()
-        );
-        self::assertIsArray($runningOrPendingRecentlyStarted);
-        self::assertSame([$runningExport2020, $pendingExport2019], $runningOrPendingRecentlyStarted);
-
-        $exports = $storage->query(
+                ->ids(['20210920']),
+            [
+                ['export', '20210920'],
+                ['list', '20210920'],
+            ],
+        ];
+        yield 'Filter job names' => [
             (new QueryBuilder())
-                ->jobs(['query-export'])
-                ->getQuery()
-        );
-        self::assertIsArray($exports);
-        self::assertSame([$pendingExport2019, $runningExport2020], $exports);
-
-        $id123 = $storage->query(
+                ->jobs(['list']),
+            [
+                ['list', '20210910'],
+                ['list', '20210915'],
+                ['list', '20210920'],
+            ],
+        ];
+        yield 'Filter statuses' => [
             (new QueryBuilder())
-                ->ids(['123'])
-                ->limit(3, 0)
-                ->getQuery()
-        );
-        self::assertIsArray($id123);
-        self::assertSame([$pendingExport2019, $completedImport2017], $id123);
+                ->statuses([BatchStatus::FAILED]),
+            [
+                ['list', '20210910'],
+            ],
+        ];
+        yield 'Order by start ASC' => [
+            (new QueryBuilder())
+                ->sort(Query::SORT_BY_START_ASC),
+            [
+                ['list', '20210910'],
+                ['list', '20210915'],
+                ['export', '20210920'],
+                ['list', '20210920'],
+                ['export', '20210922'],
+            ],
+        ];
+        yield 'Order by start DESC' => [
+            (new QueryBuilder())
+                ->sort(Query::SORT_BY_START_DESC),
+            [
+                ['export', '20210922'],
+                ['list', '20210920'],
+                ['export', '20210920'],
+                ['list', '20210915'],
+                ['list', '20210910'],
+            ],
+        ];
+        yield 'Order by end ASC' => [
+            (new QueryBuilder())
+                ->sort(Query::SORT_BY_END_ASC),
+            [
+                ['list', '20210910'],
+                ['list', '20210915'],
+                ['export', '20210920'],
+                ['list', '20210920'],
+                ['export', '20210922'],
+            ],
+        ];
+        yield 'Order by end DESC' => [
+            (new QueryBuilder())
+                ->sort(Query::SORT_BY_END_DESC),
+            [
+                ['export', '20210922'],
+                ['list', '20210920'],
+                ['export', '20210920'],
+                ['list', '20210915'],
+                ['list', '20210910'],
+            ],
+        ];
     }
 
     public function testRetrieveFilePathNotFound(): void
@@ -196,5 +231,46 @@ class FilesystemJobExecutionStorageTest extends TestCase
 
         $jobExecution = JobExecution::createRoot('123456789', 'export');
         $this->createStorage('/path/not/found')->remove($jobExecution);
+    }
+
+    public function testRemove(): void
+    {
+        $jobExecution = JobExecution::createRoot('will_be_removed', 'export');
+
+        $this->serializer->serialize($jobExecution)
+            ->shouldBeCalledTimes(1)
+            ->willReturn('serialized job execution');
+
+        $path = self::STORAGE_DIR . '/export/will_be_removed.txt';
+        $storage = $this->createStorage();
+        self::assertFileDoesNotExist($path);
+        $storage->store($jobExecution);
+        self::assertFileExists($path);
+        $storage->remove($jobExecution);
+        self::assertFileDoesNotExist($path);
+    }
+
+    public function testRemoveFileNotWritable(): void
+    {
+        $this->expectException(CannotRemoveJobExecutionException::class);
+
+        $jobExecution = JobExecution::createRoot('123456789', 'export');
+        $this->createStorage(self::READONLY_STORAGE_DIR)->remove($jobExecution);
+    }
+
+    private static function assertExecutions(array $expectedCouples, iterable $executions): void
+    {
+        $expected = [];
+        foreach ($expectedCouples as [$jobName, $executionId]) {
+            $expected[] = $jobName . '/' . $executionId;
+        }
+
+        $actual = [];
+        /** @var JobExecution $execution */
+        foreach ($executions as $execution) {
+            $actual[] = $execution->getJobName() . '/' . $execution->getId();
+        }
+
+        self::assertSame($expected, $actual);
     }
 }
