08768bd2d9ae8c7569424560b17472e3a0d3d7e6
Yann Eugoné <yeugone@prestaconcept.net>
2021-10-10 17:12:12 +0200

    Removed YAML config in favor of pure PHP & implement potential missing method on Kernel

--- tests/symfony/src/Kernel.php
+++ tests/symfony/src/Kernel.php
@@ -7,9 +7,11 @@ namespace Yokai\Batch\Sources\Tests\Symfony\App;
 use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 use Yokai\Batch\Bridge\Symfony\Framework\YokaiBatchBundle;
+use Yokai\Batch\Job\JobInterface;
 
 final class Kernel extends BaseKernel
 {
@@ -27,9 +29,50 @@ final class Kernel extends BaseKernel
         return \dirname(__DIR__);
     }
 
-    public function registerContainerConfiguration(LoaderInterface $loader): void
+    protected function configureContainer(ContainerConfigurator $container): void
     {
-        $loader->load($this->getProjectDir() . '/config/packages/');
-        $loader->load($this->getProjectDir() . '/config/services.yaml');
+        $container->extension('framework', [
+            'test' => true,
+        ]);
+        $container->extension('doctrine', [
+            'dbal' => [
+                'url' => 'sqlite:///%kernel.project_dir%/var/database.sqlite',
+                'logging' => false,
+            ],
+            'orm' => [
+                'auto_generate_proxy_classes' => true,
+                'naming_strategy' => 'doctrine.orm.naming_strategy.underscore',
+                'mappings' => [
+                    'App' => [
+                        'is_bundle' => false,
+                        'type' => 'annotation',
+                        'dir' => __DIR__ . '/Entity',
+                        'prefix' => __NAMESPACE__ . '\\Entity',
+                        'alias' => 'App',
+                    ],
+                ],
+            ],
+        ]);
+        $container->extension('yokai_batch', [
+            'storage' => [
+                'filesystem' => null,
+            ],
+        ]);
+
+        $container->services()
+            ->defaults()
+                ->autoconfigure(true)
+                ->autowire(true)
+
+            ->instanceof(JobInterface::class)
+                ->tag('yokai_batch.job')
+
+            ->load(__NAMESPACE__ . '\\', __DIR__)
+                ->exclude(__DIR__ . '/{Entity,Kernel.php}')
+        ;
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
     }
 }
