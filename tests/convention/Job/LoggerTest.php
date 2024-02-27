13c8045a4f11872c1aaf9e1c812c4182fe3fb582
Guicherd <guicherd.cedric@gmail.com>
2024-02-27 10:22:43 +0100

    #88 code review

--- tests/convention/Job/LoggerTest.php
+++ tests/convention/Job/LoggerTest.php
@@ -1,38 +1,25 @@
 <?php
 
-/*
- * This file is part of the Tucania project.
- *
- * Copyright (C) Tucania (https://www.tucania.com/) - All Rights Reserved
- *
- * For the full copyright and license information, please view the LICENSE file that was distributed with this source code.
- */
-
 declare(strict_types=1);
 
 namespace Yokai\Batch\Sources\Tests\Convention\Job;
 
 use Exception;
 use PHPUnit\Framework\TestCase;
-use Yokai\Batch\Factory\JobExecutionFactory;
 use Yokai\Batch\Factory\UniqidJobExecutionIdGenerator;
+use Yokai\Batch\JobExecution;
 
 class LoggerTest extends TestCase
 {
-    private JobExecutionFactory $jobExecutionFactory;
-
-    public function setUp(): void
-    {
-        $idGenerator = new UniqidJobExecutionIdGenerator();
-        $this->jobExecutionFactory = new JobExecutionFactory($idGenerator);
-    }
-
     public function testLogError()
     {
+        $idGenerator = new UniqidJobExecutionIdGenerator();
         $errorToLog = 'test assert logErrorMethod';
-        $jobExecution = $this->jobExecutionFactory->create('job-test');
-        $jobExecution->logError(new Exception(), $errorToLog);
+        $errorException = 'test assert errorException';
+        $jobExecution = JobExecution::createRoot($idGenerator->generate(), 'export');
+        $jobExecution->logError(new Exception($errorException), $errorToLog);
 
         self::assertStringContainsString($errorToLog, $jobExecution->getLogs()->__toString());
+        self::assertStringContainsString($errorException, $jobExecution->getLogs()->__toString());
     }
 }
