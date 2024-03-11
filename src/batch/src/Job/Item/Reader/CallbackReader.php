2c939391b78c627889506148fe377d0cce71e032
Yann Eugoné <eugone.yann@gmail.com>
2024-03-11 08:53:39 +0100

    Add class comment to CallbackReader

--- src/batch/src/Job/Item/Reader/CallbackReader.php
+++ src/batch/src/Job/Item/Reader/CallbackReader.php
@@ -6,6 +6,11 @@ namespace Yokai\Batch\Job\Item\Reader;
 
 use Yokai\Batch\Job\Item\ItemReaderInterface;
 
+/**
+ * An {@see ItemReaderInterface} that read from a {@see Closure} provided at construction.
+ *
+ * Provided {@see Closure} must accept no argument and must return an iterable of read items.
+ */
 final class CallbackReader implements ItemReaderInterface
 {
     public function __construct(
