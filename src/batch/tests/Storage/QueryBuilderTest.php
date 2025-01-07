f0db255e63a4e8e783b44a6af7318523f406d027
Yann Eugoné <eugone.yann@gmail.com>
2025-01-07 11:36:55 +0100

    Fixed checkstyle

--- src/batch/tests/Storage/QueryBuilderTest.php
+++ src/batch/tests/Storage/QueryBuilderTest.php
@@ -225,11 +225,17 @@ class QueryBuilderTest extends TestCase
             UnexpectedValueException::min(0, -1),
         ];
         yield 'QueryBuilder::startTime with inversed boundaries' => [
-            fn() => (new QueryBuilder())->startTime(new \DateTimeImmutable('2024-01-01T00:00:00+0200'), new \DateTimeImmutable('2023-12-31T23:59:59+0200')),
+            fn() => (new QueryBuilder())->startTime(
+                new \DateTimeImmutable('2024-01-01T00:00:00+0200'),
+                new \DateTimeImmutable('2023-12-31T23:59:59+0200'),
+            ),
             new UnexpectedValueException('TimeFilter expect "from" boundary to be lower than "to" boundary.'),
         ];
         yield 'QueryBuilder::endTime with inversed boundaries' => [
-            fn() => (new QueryBuilder())->endTime(new \DateTimeImmutable('2024-01-01T00:00:00+0200'), new \DateTimeImmutable('2023-12-31T23:59:59+0200')),
+            fn() => (new QueryBuilder())->endTime(
+                new \DateTimeImmutable('2024-01-01T00:00:00+0200'),
+                new \DateTimeImmutable('2023-12-31T23:59:59+0200'),
+            ),
             new UnexpectedValueException('TimeFilter expect "from" boundary to be lower than "to" boundary.'),
         ];
     }
