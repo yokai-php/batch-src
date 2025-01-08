6ef9ccf31cd44f1b9322d30a280f57bbdd20e7a9
Yann Eugoné <eugone.yann@gmail.com>
2025-01-08 11:13:56 +0100

    Fixed missing compiler pass in bundle test

--- src/batch-symfony-framework/tests/YokaiBatchBundleTest.php
+++ src/batch-symfony-framework/tests/YokaiBatchBundleTest.php
@@ -6,6 +6,7 @@ namespace Yokai\Batch\Tests\Bridge\Symfony\Framework;
 
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Yokai\Batch\Bridge\Symfony\Framework\DependencyInjection\CompilerPass\ConfigureLauncherPass;
 use Yokai\Batch\Bridge\Symfony\Framework\DependencyInjection\CompilerPass\ConfigureStoragePass;
 use Yokai\Batch\Bridge\Symfony\Framework\DependencyInjection\CompilerPass\ConfigureTemplatingPass;
 use Yokai\Batch\Bridge\Symfony\Framework\DependencyInjection\CompilerPass\RegisterJobsCompilerPass;
@@ -28,6 +29,7 @@ class YokaiBatchBundleTest extends TestCase
         }
         self::assertSame(
             [
+                ConfigureLauncherPass::class,
                 ConfigureStoragePass::class,
                 ConfigureTemplatingPass::class,
                 RegisterJobsCompilerPass::class,
