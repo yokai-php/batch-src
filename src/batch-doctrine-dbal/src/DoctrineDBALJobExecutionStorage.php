a2d8690164d40ea885fe79d3a9af6f0610edb3d4
Yann Eugoné <eugone.yann@gmail.com>
2025-11-18 09:54:43 +0100

    Fixed DBAL schema asset filter argument

--- src/batch-doctrine-dbal/src/DoctrineDBALJobExecutionStorage.php
+++ src/batch-doctrine-dbal/src/DoctrineDBALJobExecutionStorage.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Driver\Result;
 use Doctrine\DBAL\Exception as DBALException;
 use Doctrine\DBAL\Query\QueryBuilder;
+use Doctrine\DBAL\Schema\AbstractAsset;
 use Doctrine\DBAL\Schema\Comparator;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\DBAL\Types\Types;
@@ -62,7 +63,11 @@ final class DoctrineDBALJobExecutionStorage implements
     {
         $assetFilter = $this->connection->getConfiguration()->getSchemaAssetsFilter();
         $this->connection->getConfiguration()->setSchemaAssetsFilter(
-            fn(string $tableName) => $tableName === $this->table,
+            function (string|AbstractAsset $table) {
+                $table = $table instanceof AbstractAsset ? $table->getName() : $table;
+
+                return $table === $this->table;
+            },
         );
 
         $schemaManager = \method_exists($this->connection, 'createSchemaManager')
